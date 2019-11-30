de2d3c653b86f25b1736e8bd7137fb5651d9a37a
Andy Catch <andrew.catch+git@gmail.com>
2019-11-29 22:31:24 -0500

    A whole bunch of Sass 'n Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,21 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp.task('default', function() {
-  // place code for your default task here
-});
+sass.compiler = require('node-sass');
+
+gulp.task("sass", function(){
+  // Sass bit
+  // we want to run "sass css/app.scss app.css --watch"
+
+  return gulp.src("css/app.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("."))
+})
+
+gulp.task("watch", function(){
+  // we want to set Gulp to watch for the change in the .scss file
+  gulp.watch("css/app.scss", ["sass"])
+})
+
+
+gulp.task('default', ["sass"]);
